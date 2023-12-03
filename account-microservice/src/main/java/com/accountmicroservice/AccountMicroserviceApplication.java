ae622b5f1ce65b5429374826bf7caf7686816473
HussienSayed14 <hussiens399@gmail.com>
2023-12-03 21:38:01 +0200

    Initial Commit

---
+++ account-microservice/src/main/java/com/accountmicroservice/AccountMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.accountmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMicroserviceApplication.class, args);
+	}
+
+
+}
---
+++ account-microservice/src/test/java/com/accountmicroservice/AccountMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
