0c1aa826073d09f22c4431053bc50e2a6e2fffbe
HussienSayed14 <hussiens399@gmail.com>
2023-12-05 20:17:25 +0200

    Setting Up Accounts package

--- account-microservice/src/main/java/com/accountmicroservice/accounts/AccountsController.java
+++ account-microservice/src/main/java/com/accountmicroservice/accounts/AccountsController.java
@@ -1,9 +1,22 @@
 package com.accountmicroservice.accounts;
 
+import com.accountmicroservice.accounts.login.LoginService;
+import com.accountmicroservice.accounts.register.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/accounts/api/v1")
 public class AccountsController {
+    @Autowired
+    private RegisterService registerService;
+    @Autowired
+    private LoginService loginService;
+    @GetMapping("/register")
+    public ResponseEntity<?> register() {
+        return null;
+    }
 }
---
+++ account-microservice/src/main/java/com/accountmicroservice/accounts/register/RegisterService.java
@@ -0,0 +1,8 @@
+package com.accountmicroservice.accounts.register;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterService {
+}
