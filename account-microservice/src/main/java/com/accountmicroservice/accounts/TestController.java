94d21a69e2d1eda78aa5a439ec5b0b69d5e0094d
HussienSayed14 <hussiens399@gmail.com>
2023-12-15 17:29:45 +0200

    Testing jwt token

--- account-microservice/src/main/java/com/accountmicroservice/accounts/TestController.java
+++ account-microservice/src/main/java/com/accountmicroservice/accounts/TestController.java
@@ -1,5 +1,7 @@
 package com.accountmicroservice.accounts;
 
+import com.accountmicroservice.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @Autowired
+    JwtService jwtService;
     @GetMapping
     String test(@RequestHeader("Authorization") String token){
         String jwt = token.substring(7);
+        System.out.println(jwtService.extractExpiration(jwt));
         return jwt;
     }
 }
