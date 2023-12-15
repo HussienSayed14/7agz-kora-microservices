58a4933508109bc52460cad821721d3fc414e221
HussienSayed14 <hussiens399@gmail.com>
2023-12-15 22:24:39 +0200

    Adding Api Gateway

---
+++ api-gateway-microservice/src/test/java/com/apigatewaymicroservice/apigatewaymicroservice/ApiGatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigatewaymicroservice.apigatewaymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
