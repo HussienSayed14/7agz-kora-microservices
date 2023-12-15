ec5e7f538b73fccee60cf206f70dd0b569d1ca82
HussienSayed14 <hussiens399@gmail.com>
2023-12-15 23:54:22 +0200

    Adding Eureka naming server

---
+++ naming-server/naming-server/src/main/java/com/namingserver/namingserver/NamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.namingserver.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
