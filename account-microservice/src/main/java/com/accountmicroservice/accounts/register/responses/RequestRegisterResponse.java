96db800dbbe874e0c14380998a4357cad0dc7dbf
HussienSayed14 <hussiens399@gmail.com>
2023-12-17 23:27:43 +0200

    Setting Project frontend

--- account-microservice/src/main/java/com/accountmicroservice/accounts/register/responses/RequestRegisterResponse.java
+++
@@ -1,8 +0,0 @@
-package com.accountmicroservice.accounts.register.responses;
-
-import com.accountmicroservice.util.GenericResponses;
-import lombok.Getter;
-
-@Getter
-public class RequestRegisterResponse extends GenericResponses {
-}
--- account-microservice/src/main/java/com/accountmicroservice/accounts/register/responses/VerifyOtpResponse.java
+++
@@ -1,6 +0,0 @@
-package com.accountmicroservice.accounts.register.responses;
-
-import com.accountmicroservice.util.GenericResponses;
-
-public class VerifyOtpResponse extends GenericResponses {
-}
