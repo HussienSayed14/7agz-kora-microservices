7961578da638ce46d2288b2d0e85418317a9d3a0
HussienSayed14 <hussiens399@gmail.com>
2023-12-17 23:29:55 +0200

    Setting Project frontend

---
+++ Frontend-React/7agz-kora-frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+
+function App() {
+
+  return (
+    <MDBContainer fluid className="p-3 my-5 h-custom">
+
+      <MDBRow>
+
+        <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image" />
+        </MDBCol>
+
+        <MDBCol col='4' md='6'>
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+          <div className="d-flex justify-content-between mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+            <a href="!#">Forgot password?</a>
+          </div>
+
+          <div className='text-center text-md-start mt-4 pt-2'>
+            <MDBBtn className="mb-0 px-5" size='lg'>Login</MDBBtn>
+            <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href="#!" className="link-danger">Register</a></p>
+          </div>
+
+        </MDBCol>
+
+      </MDBRow>
+
+
+
+    </MDBContainer>
+  );
+}
+
+export default App;
