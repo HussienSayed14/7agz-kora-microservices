246dfc3b64657ab15e1c4d19c2e51dab3282ab8d
HussienSayed14 <hussiens399@gmail.com>
2024-01-08 23:16:26 +0200

    Setting up field microservice

---
+++ Backend-Microservices/field-microservice/src/main/java/com/fieldmicroservice/fieldmicroservice/fields/FieldsController.java
@@ -0,0 +1,9 @@
+package com.fieldmicroservice.fieldmicroservice.fields;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fields/api/v1/fields")
+public class FieldsController {
+}
