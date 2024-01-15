a99b25fc6e599293c905247905e9440676b99ec1
HussienSayed14 <hussiens399@gmail.com>
2024-01-15 20:21:43 +0200

    Adding Lombok Annotations

--- Backend-Microservices/field-microservice/src/main/java/com/fieldmicroservice/fieldmicroservice/fields/FieldsController.java
+++ Backend-Microservices/field-microservice/src/main/java/com/fieldmicroservice/fieldmicroservice/fields/FieldsController.java
@@ -1,9 +1,11 @@
 package com.fieldmicroservice.fieldmicroservice.fields;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fields/api/v1/fields")
+@RequiredArgsConstructor
 public class FieldsController {
 }
--- Backend-Microservices/field-microservice/src/main/java/com/fieldmicroservice/fieldmicroservice/fields/FieldsService.java
+++ Backend-Microservices/field-microservice/src/main/java/com/fieldmicroservice/fieldmicroservice/fields/FieldsService.java
@@ -1,8 +1,10 @@
 package com.fieldmicroservice.fieldmicroservice.fields;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FieldsService {
 
 }
