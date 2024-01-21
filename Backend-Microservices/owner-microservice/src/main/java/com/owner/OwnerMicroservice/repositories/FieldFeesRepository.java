020ede0aee58a43fb812aacb8ccffd64bcd5afa1
HussienSayed14 <hussiens399@gmail.com>
2024-01-21 23:11:53 +0200

    Adding Field Fees Repository

---
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/repositories/FieldFeesRepository.java
@@ -0,0 +1,7 @@
+package com.owner.OwnerMicroservice.repositories;
+
+import com.owner.OwnerMicroservice.entities.FieldFees;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldFeesRepository extends JpaRepository<FieldFees, Long> {
+}
