8f8aea931ff75d7fdd3baf223ef69bf301bd7c51
HussienSayed14 <hussiens399@gmail.com>
2024-01-21 21:41:35 +0200

    Adding Owner Microservice

---
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/OwnerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.owner.OwnerMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Backend-Microservices/owner-microservice/src/test/java/com/owner/OwnerMicroservice/OwnerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owner.OwnerMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
