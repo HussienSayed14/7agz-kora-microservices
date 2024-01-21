fe3652cb58337ba066ed602338972cc58a31913b
HussienSayed14 <hussiens399@gmail.com>
2024-01-21 22:28:57 +0200

    Setting Up Booking Hours Entity

---
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/entities/BookingHours.java
@@ -0,0 +1,30 @@
+package com.owner.OwnerMicroservice.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "BOOKING_HOURS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class BookingHours {
+        @Id
+        private long uid;
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long counter;
+        private int bookingDate;
+        private int price;
+        private int bookingFromTime;
+        private int bookingToTime;
+        private boolean hasOffer;
+        private int offerPercentage;
+        private int offerPrice;
+        private boolean isBooked;
+        private String bookingStatus; // Available, Booked, Locked
+        private String hourStatus; // Available, Pending, Unavailable
+        private int fee;
+
+}
