f25b0f7e1412be68eedf3af463bb744ae7bc9b05
HussienSayed14 <hussiens399@gmail.com>
2024-01-21 23:12:44 +0200

    Adding Field Owner Repository

---
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/repositories/FieldOwnerRepository.java
@@ -0,0 +1,7 @@
+package com.owner.OwnerMicroservice.repositories;
+
+import com.owner.OwnerMicroservice.entities.FieldOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldOwnerRepository extends JpaRepository<FieldOwner, String> {
+}
