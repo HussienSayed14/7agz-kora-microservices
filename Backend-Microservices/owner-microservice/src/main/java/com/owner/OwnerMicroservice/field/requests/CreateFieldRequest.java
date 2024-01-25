529260e6ff1cce893d3e3ecab229b59d6a9e33f0
HussienSayed14 <hussiens399@gmail.com>
2024-01-26 00:53:41 +0200

    Adding Basic Validations

--- Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/field/requests/CreateFieldRequest.java
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/field/requests/CreateFieldRequest.java
@@ -1,5 +1,6 @@
 package com.owner.OwnerMicroservice.field.requests;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +8,13 @@ import lombok.Setter;
 @Setter
 public class CreateFieldRequest {
 
+    @NotNull(message = "Field name is mandatory")
     private String fieldName;
+    @NotNull(message = "Field type is mandatory")
     private String fieldType;
+    @NotNull(message = "Field size is mandatory")
     private String fieldSize;
+    @NotNull(message = "Field location is mandatory")
     private String fieldLocation;
     private int hourPrice;
-    private int rating;
 }
