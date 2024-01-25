3c01fd8fb4e6f6538a0215aee40b004190b44363
HussienSayed14 <hussiens399@gmail.com>
2024-01-26 00:51:03 +0200

    Adding Owner Controller endpoints

--- Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/field/FieldController.java
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/field/FieldController.java
@@ -3,10 +3,7 @@ package com.owner.OwnerMicroservice.field;
 import com.owner.OwnerMicroservice.field.requests.CreateFieldRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/owner/api/v1/field")
@@ -19,6 +16,17 @@ public class FieldController {
     public ResponseEntity createField(@RequestBody CreateFieldRequest request){
         return fieldService.createField(request);
     }
+
+    @GetMapping("/getField/{fieldId}")
+    public ResponseEntity getFieldById(@PathVariable String fieldId){
+        return fieldService.getFieldById(fieldId);
+    }
+
+    @GetMapping("/getAllFields")
+    public ResponseEntity getAllOwnerFields(@RequestHeader(value = "Authorization") String bearerToken){
+        return fieldService.getAllOwnerFields();
+    }
+
 
 
 
