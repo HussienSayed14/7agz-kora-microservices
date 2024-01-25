6a61fae5739f1e32b232182dde61e3603bfdfbeb
HussienSayed14 <hussiens399@gmail.com>
2024-01-25 20:07:52 +0200

    Adding Create Field Service

--- Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/createField/CreateFieldController.java
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/createField/CreateFieldController.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreateFieldController {
 
 
+
+
 }
--- Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/createField/CreateFieldService.java
+++ Backend-Microservices/owner-microservice/src/main/java/com/owner/OwnerMicroservice/createField/CreateFieldService.java
@@ -1,4 +1,10 @@
 package com.owner.OwnerMicroservice.createField;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CreateFieldService {
 }
